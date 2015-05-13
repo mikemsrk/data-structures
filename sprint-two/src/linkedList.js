2648b474d23a30a28b688761664c35acb2079500
mikemsrk <min2033@gmail.com>
2015-05-13 16:31:44 -0700

    complete sprint 2: linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,44 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = new Node(value);
+
+    if (list.tail !== null){
+      list.tail.next = node;
+    }
+
+    list.tail = node;
+
+    if (list.head === null) {
+      list.head = list.tail;
+    }
+
   };
 
   list.removeHead = function(){
+    var result = list.head.value;
+
+    list.head = list.head.next;
+
+    return result;
   };
 
   list.contains = function(target){
+    var result = false;
+
+    var element = list.head;
+
+    while (element !== null) {
+      if (element.value === target) {
+        result = true;
+        break;
+      }
+
+      element = element.next;
+    }
+
+    return result;
+
   };
 
   return list;
