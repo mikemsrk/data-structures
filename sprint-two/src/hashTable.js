e3e2607b06477438066188d804ebda225e403bcd
mikemsrk <min2033@gmail.com>
2015-05-14 11:27:01 -0700

    Complete hash table.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,34 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  if(this._storage[i] === null || this._storage[i] === undefined){
+    this._storage[i] = {};
+  }
+  this._storage[i][k] = v;
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  if(this._storage[i] === null){
+    return null;
+  }else{
+    return this._storage[i][k];
+  }
 };
 
 HashTable.prototype.remove = function(k){
 
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  delete this._storage[i][k];
+
+  var count = 0;
+  for(var key in this._storage[i]){
+    count++;
+  }
+  if(count === 0){
+    this._storage[i] = null;
+  }
 };
 
 
